45001dbc2e3219c271392e1bacf2add300bf544a
Celeo <mattboulanger@fastmail.com>
2020-06-30 19:07:12 -0700

    Alliance endpoints

--- src/groups/alliance.rs
+++ src/groups/alliance.rs
@@ -7,12 +7,20 @@ pub struct AllianceGroup<'a> {
 
 #[derive(Debug, Deserialize)]
 pub struct AllianceInfo {
-    creator_corporation_id: u64,
-    creator_id: u64,
-    date_founded: String,
-    executor_corporation_id: u64,
-    name: String,
-    ticker: String,
+    pub creator_corporation_id: u64,
+    pub creator_id: u64,
+    pub date_founded: String,
+    pub executor_corporation_id: u64,
+    pub name: String,
+    pub ticker: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AllianceIcons {
+    #[serde(rename = "128x128")]
+    pub p128x128: String,
+    #[serde(rename = "64x64")]
+    pub p64x64: String,
 }
 
 impl<'a> AllianceGroup<'a> {
@@ -28,8 +36,30 @@ impl<'a> AllianceGroup<'a> {
     pub async fn get_info(&self, id: u64) -> Result<AllianceInfo, EsiError> {
         let path = self
             .esi
-            .get_endpoint_for_op_id("get_alliances_alliance_id")?;
-        let path = path.replace("{alliance_id}", &id.to_string());
+            .get_endpoint_for_op_id("get_alliances_alliance_id")?
+            .replace("{alliance_id}", &id.to_string());
+        self.esi
+            .query("GET", RequestType::Public, &path, None, None)
+            .await
+    }
+
+    /// Get list of corporation IDs in an alliance by ID.
+    pub async fn get_alliance_corporations(&self, alliance_id: u64) -> Result<Vec<u64>, EsiError> {
+        let path = self
+            .esi
+            .get_endpoint_for_op_id("get_alliances_alliance_id_corporations")?
+            .replace("{alliance_id}", &alliance_id.to_string());
+        self.esi
+            .query("GET", RequestType::Public, &path, None, None)
+            .await
+    }
+
+    /// Get paths to the alliance's icons on the image server, by ID.
+    pub async fn get_alliance_icons(&self, id: u64) -> Result<AllianceIcons, EsiError> {
+        let path = self
+            .esi
+            .get_endpoint_for_op_id("get_alliances_alliance_id_icons")?
+            .replace("{alliance_id}", &id.to_string());
         self.esi
             .query("GET", RequestType::Public, &path, None, None)
             .await
