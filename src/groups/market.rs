0f82c1b6e2a337ab8d73c3a62905b61e36770baa
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-03-09 19:33:12 -0800

    Add a few endpoints to universe and market group

--- src/groups/market.rs
+++ src/groups/market.rs
@@ -2,7 +2,59 @@
 
 use crate::prelude::*;
 
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct HistoryItem {
+    pub average: f64,
+    pub date: String,
+    pub highest: f64,
+    pub lowest: f64,
+    pub order_count: i64,
+    pub volume: i64,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct MarketOrder {
+    pub duration: i32,
+    pub is_buy_order: bool,
+    pub issued: String,
+    pub location_id: i64,
+    pub min_volume: i32,
+    pub order_id: i64,
+    pub price: f64,
+    pub range: String,
+    pub system_id: i32,
+    pub type_id: i32,
+    pub volume_remain: i32,
+    pub volume_total: i32,
+}
+
 /// Endpoints for Market
 pub struct MarketGroup<'a> {
     pub(crate) esi: &'a Esi,
+}
+
+impl<'a> MarketGroup<'a> {
+    api_get!(
+        /// Get a list of historical market statistics for the specified type in a region
+        get_region_history,
+        "get_markets_region_id_history",
+        RequestType::Public,
+        Vec<HistoryItem>,
+        (region_id: i32) => "{region_id}";
+        (type_id: i32) => "type_id"
+    );
+
+    api_get!(
+        /// Get a list of orders in a region
+        get_region_orders,
+        "get_markets_region_id_orders",
+        RequestType::Public,
+        Vec<MarketOrder>,
+        (region_id: i32) => "{region_id}";
+        Optional(order_type: String) => "order_type",
+        Optional(page: i32) => "page",
+        Optional(type_id: i32) => "type_id"
+    );
 }
