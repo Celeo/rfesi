3bb89e09b30d6944c148605126bda07834e6a204
Olivier Ory <mail@normegil.be>
2023-11-13 22:32:31 +0100

    Search Endpoint

--- src/groups/search.rs
+++ src/groups/search.rs
@@ -6,3 +6,33 @@ use crate::prelude::*;
 pub struct SearchGroup<'a> {
     pub(crate) esi: &'a Esi,
 }
+
+impl<'a> SearchGroup<'a> {
+    api_get!(
+        /// Search for entities that match a given sub-string.
+        search,
+        "get_characters_character_id_search",
+        RequestType::Authenticated,
+        SearchResult,
+        (character_id: i32) => "{character_id}";
+        (categories: String) => "categories",
+        (search: String) => "search";
+        Optional(strict: bool) => "strict"
+    );
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct SearchResult {
+    pub agent: Option<Vec<i32>>,
+    pub alliance: Option<Vec<i32>>,
+    pub character: Option<Vec<i32>>,
+    pub constellation: Option<Vec<i32>>,
+    pub corporation: Option<Vec<i32>>,
+    pub faction: Option<Vec<i32>>,
+    pub inventory_type: Option<Vec<i32>>,
+    pub region: Option<Vec<i32>>,
+    pub solar_system: Option<Vec<i32>>,
+    pub station: Option<Vec<i32>>,
+    pub structure: Option<Vec<i32>>,
+}
