5cf84af94991e6ce4d3dd4373ba6d53348c25aaa
Olivier Ory <olivier.ory@uliege.be>
2023-12-06 15:18:23 +0100

    Wallet - Add balance endpoint

--- src/groups/wallet.rs
+++ src/groups/wallet.rs
@@ -6,3 +6,16 @@ use crate::prelude::*;
 pub struct WalletGroup<'a> {
     pub(crate) esi: &'a Esi,
 }
+
+impl<'a> WalletGroup<'a> {
+
+    api_get!(
+        /// Returns a character’s wallet balance
+        get_wallet,
+        "get_characters_character_id_wallet",
+        RequestType::Authenticated,
+        f64,
+        (character_id: i32) => "{character_id}"
+    );
+
+}
