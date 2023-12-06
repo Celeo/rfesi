ff153a15543b4a5151e68c9c5671cd3bec2a6b44
Olivier Ory <mail@normegil.be>
2023-12-06 22:14:42 +0100

    Character Orders

--- src/groups/market.rs
+++ src/groups/market.rs
@@ -38,6 +38,25 @@ pub struct PriceItem {
     pub type_id: i32,
 }
 
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct CharacterOrder {
+    pub duration: i32,
+    pub escrow: Option<f64>,
+    pub is_buy_order: Option<bool>,
+    pub is_corporation: bool,
+    pub issued: String,
+    pub location_id: i64,
+    pub min_volume: Option<i32>,
+    pub order_id: i64,
+    pub price: f64,
+    pub range: String,
+    pub region_id: i32,
+    pub type_id: i32,
+    pub volume_remain: i32,
+    pub volume_total: i32,
+}
+
 /// Endpoints for Market
 pub struct MarketGroup<'a> {
     pub(crate) esi: &'a Esi,
@@ -72,5 +91,14 @@ impl<'a> MarketGroup<'a> {
         "get_markets_prices",
         RequestType::Public,
         Vec<PriceItem>,
+    );
+
+    api_get!(
+        /// List open market orders placed by a character
+        get_character_orders,
+        "get_characters_character_id_orders",
+        RequestType::Authenticated,
+        Vec<CharacterOrder>,
+        (character_id: u64) => "{character_id}"
     );
 }
