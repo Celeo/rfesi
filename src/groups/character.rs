6f6e50e524c1e91a28108f691a5521459922e990
Amon Forstmann <info@forstmann.biz>
2023-12-24 12:19:52 +0100

    adds structs and endpoint for character skills

--- src/groups/character.rs
+++ src/groups/character.rs
@@ -60,6 +60,23 @@ pub struct Blueprint {
     pub type_id: u64,
 }
 
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct Skill {
+    pub skill_id: u32,
+    pub active_skill_level: u32,
+    pub skillpoints_in_skill: u64,
+    pub trained_skill_level: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct Skills {
+    pub skills: Vec<Skill>,
+    pub total_sp: u64,
+    pub unallocated_sp: u32,
+}
+
 impl<'a> CharacterGroup<'a> {
     api_get!(
         /// Get a character's public information.
@@ -104,6 +121,15 @@ impl<'a> CharacterGroup<'a> {
         "get_characters_character_id_blueprints",
         RequestType::Authenticated,
         Vec<Blueprint>,
+        (character_id: u64) => "{character_id}"
+    );
+
+    api_get!(
+        /// Get character skills.
+        get_skills,
+        "get_characters_character_id_skills",
+        RequestType::Authenticated,
+        Skills,
         (character_id: u64) => "{character_id}"
     );
 
