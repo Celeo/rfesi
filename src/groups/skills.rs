bdfc85e80b7623827678aefcd149743a8f3a9fb6
Amon Forstmann <amonforstmann@users.noreply.github.com>
2023-12-24 19:53:14 +0100

    adds structs and endpoint for character skills (#32)
    * adds structs and endpoint for character skills
    
    * Move new endpoint to skills.rs
    
    ---------
    
    Co-authored-by: Celeo <mattboulanger@fastmail.com>

--- src/groups/skills.rs
+++ src/groups/skills.rs
@@ -1,8 +1,34 @@
-#![allow(unused)]
-
 use crate::prelude::*;
+
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct Skill {
+    pub skill_id: u32,
+    pub active_skill_level: u32,
+    pub skillpoints_in_skill: u64,
+    pub trained_skill_level: u32,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct Skills {
+    pub skills: Vec<Skill>,
+    pub total_sp: u64,
+    pub unallocated_sp: u32,
+}
 
 /// Endpoints for Skills
 pub struct SkillsGroup<'a> {
     pub(crate) esi: &'a Esi,
+}
+
+impl<'a> SkillsGroup<'a> {
+    api_get!(
+        /// Get character skills.
+        get_skills,
+        "get_characters_character_id_skills",
+        RequestType::Authenticated,
+        Skills,
+        (character_id: u64) => "{character_id}"
+    );
 }
