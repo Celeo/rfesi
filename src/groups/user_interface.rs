cae755289b275c6b56f3423e618dd8e91227644a
Celeo <mattboulanger@fastmail.com>
2024-11-10 12:21:08 -0800

    Add open market details window endpoint

--- src/groups/user_interface.rs
+++ src/groups/user_interface.rs
@@ -1,8 +1,25 @@
-#![allow(unused)]
-
 use crate::prelude::*;
 
 /// Endpoints for UserInterface
 pub struct UserInterfaceGroup<'a> {
     pub(crate) esi: &'a Esi,
+}
+
+impl UserInterfaceGroup<'_> {
+    /// Open the market details window.
+    pub async fn open_market_details_window(
+        &self,
+        character_id: u64,
+        type_id: i32,
+    ) -> EsiResult<()> {
+        // not using the macro since it doesn't like no body
+        let path = self
+            .esi
+            .get_endpoint_for_op_id("post_ui_openwindow_marketdetails")?
+            .replace("{character_id}", &character_id.to_string())
+            .replace("{type_id}", &type_id.to_string());
+        self.esi
+            .query("GET", RequestType::Public, &path, None, None)
+            .await
+    }
 }
