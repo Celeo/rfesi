18e5ab261bbcf3baf9be3cdd160d04efdc6fe6b8
Celeo <mattboulanger@fastmail.com>
2025-09-12 20:04:31 -0700

    Remove unneeded parameter for UI endpoint

--- src/groups/user_interface.rs
+++ src/groups/user_interface.rs
@@ -7,16 +7,11 @@ pub struct UserInterfaceGroup<'a> {
 
 impl UserInterfaceGroup<'_> {
     /// Open the market details window.
-    pub async fn open_market_details_window(
-        &self,
-        character_id: u64,
-        type_id: i32,
-    ) -> EsiResult<()> {
+    pub async fn open_market_details_window(&self, type_id: i32) -> EsiResult<()> {
         // not using the macro since it doesn't like no body
         let path = self
             .esi
             .get_endpoint_for_op_id("post_ui_openwindow_marketdetails")?
-            .replace("{character_id}", &character_id.to_string())
             .replace("{type_id}", &type_id.to_string());
         self.esi
             .query("POST", RequestType::Authenticated, &path, None, None)
